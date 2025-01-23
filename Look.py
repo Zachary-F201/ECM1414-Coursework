938322b84803b3b51af7f8ae38a7111103118d6c
sjjleachexeter <sl1169@exeter.ac.uk>
2025-01-23 15:05:19 +0000

    Look

---
+++ Look.py
@@ -0,0 +1,8 @@
+from main.py import request
+
+#In general, we will define the direction for up as True, and down as false, and return these as boolean values
+def direction():
+    if 
+
+#the function deals with... it's in the name
+def look_functionality()
