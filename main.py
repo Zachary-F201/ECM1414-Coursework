8e6de3a68db1c9cf38effbb53c6a30d77030dde6
Zachary-F201 <zjmf201@exeter.ac.uk>
2025-01-23 14:52:52 +0000

    slight fixes with request and lift_movement

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 pending_requests = []
 
 def request(floor, destination):
-    pending_requests.append([floor,destination])
+    pending_requests.push([floor,destination])
 
-def lift movement(algorithm = "SCAN"):
+def lift movement(algorithm = "SCAN", current_floor):
     if algorithm == "SCAN"
         #Call SCAN algorithm, defined elsewhere
     elif algorithm == "LOOK"
