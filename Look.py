75b4bcf479a8aee03c5c1c025bd5593c1902ad8c
sjjleachexeter <sl1169@exeter.ac.uk>
2025-01-23 15:15:08 +0000

    direction of travel function created

--- Look.py
+++ Look.py
@@ -1,8 +1,5 @@
-from main.py import request
+from main.py import request and lift_movement
 
-#In general, we will define the direction for up as True, and down as false, and return these as boolean values
-def direction():
-    if 
 
 #the function deals with... it's in the name
 def look_functionality()
--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def lift_movement(algorithm = "SCAN", current_floor):
     elif algorithm == "Real-Time":
         #... ...
     #Should return a destination
+
+#In general, we will define the direction for up as +1, and down as -1, and for it being stationary as 0 and return these as boolean values
+def direction_of_travel():
+    if current_floor < destination:
+        direction_of_travel = 1
+    elif current_floor > destination:
+        direction_of_travel = -1
+    else:
+        direction_of_travel = 0
+    return direction_of_travel
