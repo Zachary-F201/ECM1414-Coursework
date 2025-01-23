b167196dd27effae6957d2300636dcac156fda2d
Zachary-F201 <zjmf201@exeter.ac.uk>
2025-01-23 14:49:49 +0000

    Basic lift idea

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ pending_requests = []
 
 def request(floor, destination):
     pending_requests.append([floor,destination])
+
+def lift movement(algorithm = "SCAN"):
+    if algorithm == "SCAN"
+        #Call SCAN algorithm, defined elsewhere
+    elif algorithm == "LOOK"
+        #Call LOOK algorithm, defined elsewhere
+    elif algorithm == "Real-Time":
+        #... ...
+    #Should return a destination
