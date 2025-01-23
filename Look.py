4373978553006d388cd03bbc3785abb593588153
sjjleachexeter <sl1169@exeter.ac.uk>
2025-01-23 15:25:33 +0000

    handles look functionality

--- Look.py
+++ Look.py
@@ -1,5 +1,12 @@
-from main.py import request and lift_movement
+from main.py import request and lift_movement and direction_of_travel
 
 
 #the function deals with... it's in the name
 def look_functionality()
+    #checks if the lift if heading in the direction of the target floor
+    if direction_of_travel == 1 and destination > current_floor or direction_of_travel == -1 and destination < current_floor:
+        pass
+    #this handles the case where it isn't heading in the right direction
+    elif direction_of_travel == 1 and destination < current_floor or direction_of_travel == -1 and destination > current_floor:
+        direction_of_travel = -(direction_of_travel)
+    return direction_of_travel
